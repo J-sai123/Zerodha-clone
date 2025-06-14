7d4ea6188c6e50d011966d8e102b1efaf55ce5f5
J Sai Rakshith <sairakshith5683@gmail.com>
2025-06-14 11:10:00 +0530

    Update

--- backend/model/HoldingsModel.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { model } from 'mongoose';
-import HoldingsSchema from '../schemas/HoldingsSchema';
-// Define Holdings type here if the module is missing
-
-
-
-const HoldingsModel = mongoose.model('Holding', HoldingsSchema);
-export default HoldingsModel;
--- backend/model/OrdersModel.ts
+++
@@ -1,8 +0,0 @@
-import { model } from 'mongoose';
-import OrdersSchema from '../schemas/OrdersSchema';
-// Define Holdings type here if the module is missing
-
-
-
-const OrdersModel = model('Orders', OrdersSchema);
-export default OrdersModel;
